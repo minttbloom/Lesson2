921e96f9cdf73832367491d7af407ba38cfe4e04
Kate Bourakovskaia <mintt.bloom@icloud.com>
2021-11-23 14:44:50 +0300

    edit

--- functions/main.go
+++ functions/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func boolfunc1(y1 int) bool {
 	var x1 = 2
-	if (x1 == y1) {
-	return true
-} else {
+	if x1 == y1 {
+		return true
+	} else {
 		return false
 	}
 }
@@ -19,8 +19,8 @@ func boolfunc2(x2, y2 bool) bool {
 	}
 }
 
-func boollfunc4(x, y, R int) bool{
-	var res = (x-x*0)^2+(y-y*0)^2<=R^2
+func boollfunc4(x, y, R int) bool {
+	var res = (x - x * 0) ^ 2 + (y - y * 0) ^ 2 <= R ^ 2
 	if res {
 		return true
 	} else {
@@ -29,7 +29,7 @@ func boollfunc4(x, y, R int) bool{
 }
 
 func frombooltostrlfunc(x3, y3, R int) string {
-	if (x3-x3*0)^2+(y3-y3*0)^2<=R^2 {
+	if (x3 - x3 * 0) ^ 2 + (y3 - y3 * 0) ^ 2 <= R ^ 2 {
 		return "true"
 	} else {
 		return "false"
@@ -39,7 +39,7 @@ func frombooltostrlfunc(x3, y3, R int) string {
 func comparefunc1(c1 int) bool {
 	if c1 >= 0 {
 		return true
-	}else {
+	} else {
 		return false
 	}
 }
@@ -56,8 +56,7 @@ func comparefunc2(c2 int) string {
 	}
 }
 
-func comparefunc3(с3 int, km string) string {
-	var c3 int
+func comparefunc3(c3 int) string {
 	if (c3 <= 70000000) && (c3 >= 120000000) {
 		return "The nearest plamet is Mercury"
 	} else if (c3 <= 120000001) && (c3 >= 180000000) {
@@ -74,7 +73,7 @@ func comparefunc3(с3 int, km string) string {
 		return "The nearest plamet is Uranus"
 	} else if (c3 <= 43000000001) && (c3 >= 8000000000) {
 		return "The nearest plamet is Neptune"
-	} else if (c3 >= 8000000001) {
+	} else if c3 >= 8000000001 {
 		return "It is not a solar system"
 	} else {
 		return ""
@@ -82,27 +81,37 @@ func comparefunc3(с3 int, km string) string {
 }
 
 func comparefunc4(c4 int, c44 int) string {
-	if (c4 >= 0) && (c4 <= 20) && (c44 >= 1500) && (c44 <=3000) {
+	if (c4 >= 0) && (c4 <= 20) && (c44 >= 1500) && (c44 <= 3000) {
 		return "The first gear"
-	} else if (c4 >= 21) && (c4 <= 35) && (c44 >= 1500) && (c44 <=3000) {
+	} else if (c4 >= 21) && (c4 <= 35) && (c44 >= 1500) && (c44 <= 3000) {
 		return "The second gear"
-	} else if (c4 >= 36) && (c4 <= 50) && (c44 >= 1500) && (c44 <=3000) {
+	} else if (c4 >= 36) && (c4 <= 50) && (c44 >= 1500) && (c44 <= 3000) {
 		return "The third gear"
-	} else if (c4 >= 51) && (c4 <= 65) && (c44 >= 1500) && (c44 <=3000) {
+	} else if (c4 >= 51) && (c4 <= 65) && (c44 >= 1500) && (c44 <= 3000) {
 		return "The fourth gear"
-	} else if (c4 >= 66) && (c4 <= 80) && (c44 >= 1500) && (c44 <=3000) {
+	} else if (c4 >= 66) && (c4 <= 80) && (c44 >= 1500) && (c44 <= 3000) {
 		return "The fifth gear"
-	} else if (c44 <= 1499) {
+	} else if c44 <= 1499 {
 		return "You need to shift into a lower gear"
-	}  else if (c44 >= 3001) {
+	}  else if c44 >= 3001 {
 		return "You need to shift into a higher gear"
 	} else {
 		return ""
 	}
 }
 
-func main() {
+func signalStrenght( distance int) int {
+	var signalOriginPerCent = 100
+	var km = 100
+	var flowRatePerCent = 2
+	return signalOriginPerCent - distance / km * flowRatePerCent
+}
 
+func downloadTime(gig float64, intSpeed float64) float64 {
+	return (gig * 1000) / (intSpeed / 8)
+}
+
+func main() {
 	fmt.Println(boolfunc1(2))
 	fmt.Println(boolfunc2(false, false))
 	fmt.Println(frombooltostrlfunc(4,4,2))
@@ -110,8 +119,11 @@ func main() {
 
 	fmt.Println(comparefunc1(1))
 	fmt.Println(comparefunc2(86))
-	fmt.Println(comparefunc3(10000000000, "km"))
+	fmt.Println(comparefunc3(10000000000))
 	fmt.Println(comparefunc4(40, 3050))
+
+	fmt.Println(signalStrenght(600))
+	fmt.Println(downloadTime(1, 100))
 
 
 }
