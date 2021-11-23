11b5d50fd07361450e141998d021288d44f7378d
Kate Bourakovskaia <mintt.bloom@icloud.com>
2021-11-23 14:51:50 +0300

    slices

---
+++ slices/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func x(a ...int) {
+	var nums = []int {
+		3, 5, 7,
+	}
+	for _, v := range nums {
+		correctValue := v - 1
+		if len(a) > correctValue {
+			fmt.Print(a[correctValue])
+		} else {
+			fmt.Print(0)
+		}
+	}
+}
+
+func main() {
+	x(1, 2, 3, 4, 5)
+}
