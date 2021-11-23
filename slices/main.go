57c8a3def42acc26d769ef637f514cf2805cad15
Kate Bourakovskaia <mintt.bloom@icloud.com>
2021-11-23 14:53:43 +0300

    slice

--- slices/main.go
+++ slices/main.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func x(a ...int) {
-	var nums = []int {
-		3, 5, 7,
-	}
+	var nums = []int { 3, 5, 7 }
 	for _, v := range nums {
 		correctValue := v - 1
 		if len(a) > correctValue {
